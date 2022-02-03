759cea4b2470b7cef79087992430ef6e354f89d2
artvvr <artur.metelskiy@gmail.com>
2022-02-03 17:17:39 +0300

    feat: set up eslint and created the project structure

--- rslang/src/app/app.component.spec.ts
+++ rslang/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +10,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'rslang'`, () => {
+  it("should have as title 'rslang'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rslang');
+    expect(app.title).toEqual("rslang");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('rslang app is running!');
+    expect(compiled.querySelector(".content span")?.textContent).toContain("rslang app is running!");
   });
 });
--- rslang/src/app/app.module.ts
+++ rslang/src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MainPageComponent } from './main-page/main-page.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
+import { StatisticsPageComponent } from './pages/statistics-page/statistics-page.component';
+import { GameOasisComponent } from './pages/mini-games/game-oasis/game-oasis.component';
+import { GameSavannahComponent } from './pages/mini-games/game-savannah/game-savannah.component';
+import { GameSprintComponent } from './pages/mini-games/game-sprint/game-sprint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    BookPageComponent,
+    StatisticsPageComponent,
+    GameOasisComponent,
+    GameSavannahComponent,
+    GameSprintComponent
   ],
   imports: [
     BrowserModule
