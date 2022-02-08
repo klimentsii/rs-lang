254377483f32bb2f82446ac0f99df973c3bf69c3
artvvr <artur.metelskiy@gmail.com>
2022-02-08 23:42:27 +0300

    feat: add page with mini-games

--- rslang/src/app/app.component.ts
+++ rslang/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  styles: [":host { width: 100% }"]
+  styles: [":host { width: 100%; }"]
 })
 export class AppComponent {
   title = "RS-lang";
