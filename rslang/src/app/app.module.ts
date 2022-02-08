24a59435ded588f394988b7288577681486bf94f
artvvr <artur.metelskiy@gmail.com>
2022-02-08 19:03:46 +0300

    fix: routing

--- rslang/src/app/app.module.ts
+++ rslang/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { Routes, RouterModule } from "@angular/router";
+
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./pages/main-page/main-page.component";
 import { BookPageComponent } from "./pages/book-page/book-page.component";
@@ -9,6 +11,14 @@ import { GameOasisComponent } from "./pages/mini-games/game-oasis/game-oasis.com
 import { GameSavannahComponent } from "./pages/mini-games/game-savannah/game-savannah.component";
 import { GameSprintComponent } from "./pages/mini-games/game-sprint/game-sprint.component";
 import { HeaderComponent } from "./pages/header-component/header.component";
+import { PageGamesComponent } from "./pages/page-games/page-games.component";
+
+const appRoutes: Routes = [
+  { path: "home", component: MainPageComponent },
+  { path: "statistics", component: StatisticsPageComponent },
+  { path: "games", component: PageGamesComponent },
+  { path: "book", component: BookPageComponent },
+];
 
 @NgModule({
   declarations: [
@@ -22,7 +32,8 @@ import { HeaderComponent } from "./pages/header-component/header.component";
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
