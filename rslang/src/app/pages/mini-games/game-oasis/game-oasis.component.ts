38a75008f0350ae2c0ace86a0b25b4073e493f55
klimentsii <klimparh@gmail.com>
2022-02-08 19:06:42 +0300

    feat: none

--- rslang/src/app/pages/mini-games/game-oasis/game-oasis.component.ts
+++ rslang/src/app/pages/mini-games/game-oasis/game-oasis.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-game-oasis',
-  templateUrl: './game-oasis.component.html',
-  styleUrls: ['./game-oasis.component.css']
+  selector: "app-game-oasis",
+  templateUrl: "./game-oasis.component.html",
+  styleUrls: ["./game-oasis.component.css"]
 })
 export class GameOasisComponent implements OnInit {
 
