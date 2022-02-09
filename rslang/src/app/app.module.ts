530aa12e6ff815438d11432ab271418fd748d7e3
artvvr <artur.metelskiy@gmail.com>
2022-02-09 22:13:47 +0300

    refactor: removed unnecessary files and added the necessary directives

--- rslang/src/app/app.module.ts
+++ rslang/src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { HttpClientModule } from "@angular/common/http";
 import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./pages/main-page/main-page.component";
 import { BookPageComponent } from "./pages/book-page/book-page.component";
 import { StatisticsPageComponent } from "./pages/statistics-page/statistics-page.component";
-import { GameOasisComponent } from "./pages/mini-games/game-oasis/game-oasis.component";
-import { GameSavannahComponent } from "./pages/mini-games/game-savannah/game-savannah.component";
 import { GameSprintComponent } from "./pages/mini-games/game-sprint/game-sprint.component";
 import { HeaderComponent } from "./pages/header-component/header.component";
 import { PageGamesComponent } from "./pages/page-games/page-games.component";
+import { GameAudioCallComponent } from "./pages/mini-games/game-audio-call/game-audio-call.component";
 
 const appRoutes: Routes = [
   { path: "", component: MainPageComponent },
   { path: "statistics", component: StatisticsPageComponent },
   { path: "games", component: PageGamesComponent },
   { path: "book", component: BookPageComponent },
+  { path: "games/sprint", component: GameSprintComponent },
+  { path: "games/audio-call", component: GameAudioCallComponent },
 ];
 
 @NgModule({
@@ -26,15 +29,16 @@ const appRoutes: Routes = [
     MainPageComponent,
     BookPageComponent,
     StatisticsPageComponent,
-    GameOasisComponent,
-    GameSavannahComponent,
     GameSprintComponent,
     HeaderComponent,
-    PageGamesComponent
+    PageGamesComponent,
+    GameAudioCallComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ rslang/src/app/pages/mini-games/game-audio-call/game-audio-call.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-audio-call',
+  templateUrl: './game-audio-call.component.html',
+  styleUrls: ['./game-audio-call.component.css']
+})
+export class GameAudioCallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
