5d01d9cc3e8bd69ff836735f561c69572e30178c
artvvr <artur.metelskiy@gmail.com>
2022-02-13 23:47:34 +0300

    init: start mini-game: 'Sprint'

--- rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
+++ rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
+
+
+import { dataBase } from "../../../interfaces/interfaces";
+import { URL, MAX_WORDS, MAX_DATA, LAST_PAGE, FIRST_PAGE } from "../../../constants/constants";
 
 @Component({
-  selector: 'app-game-sprint',
-  templateUrl: './game-sprint.component.html',
-  styleUrls: ['./game-sprint.component.css']
+  selector: "app-game-sprint",
+  templateUrl: "./game-sprint.component.html",
+  styleUrls: ["./game-sprint.component.css"]
 })
 export class GameSprintComponent implements OnInit {
 
-  constructor() { }
+  db: dataBase = [];
+
+  constructor(protected _location: Location) { }
 
   ngOnInit(): void {
+    this.getDb();
+  }
+
+  getDb(): void {
+    this.db = JSON.parse(localStorage.getItem("db") as string);
+
+    if (this.db === null || undefined) this._location.back();
+
+    localStorage.clear();
   }
 
 }
