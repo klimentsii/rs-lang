12c97aee821699ef5db302526ef57359e86587af
artvvr <artur.metelskiy@gmail.com>
2022-02-14 02:28:10 +0300

    feat: made a working functionality

--- rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
+++ rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
@@ -3,7 +3,7 @@ import { Location } from "@angular/common";
 
 
 import { dataBase } from "../../../interfaces/interfaces";
-import { URL, MAX_WORDS, MAX_DATA, LAST_PAGE, FIRST_PAGE } from "../../../constants/constants";
+import { URL, MAX_WORDS, MAX_DATA, LAST_PAGE, FIRST_PAGE, randN, elseRandN } from "../../../constants/constants";
 
 @Component({
   selector: "app-game-sprint",
@@ -14,6 +14,19 @@ export class GameSprintComponent implements OnInit {
 
   db: dataBase = [];
 
+  word!: string;
+
+  wordTranslate!: string;
+
+  points: number = 0;
+
+  bonus: number = 0;
+
+  timeOut: number = 5;
+
+  r1!: number;
+  r2!: number;
+
   constructor(protected _location: Location) { }
 
   ngOnInit(): void {
@@ -23,9 +36,61 @@ export class GameSprintComponent implements OnInit {
   getDb(): void {
     this.db = JSON.parse(localStorage.getItem("db") as string);
 
-    if (this.db === null || undefined) this._location.back();
-
     localStorage.clear();
+
+    if (this.db) {
+      this.assignDataWords();
+      this.timeOfGame();
+    } else {
+      this._location.back();
+    }
   }
 
+  assignDataWords() {
+    this.r1 = randN();
+    this.r2 = elseRandN();
+
+    this.word = this.db[this.r1].word;
+    this.wordTranslate = this.db[this.r2].wordTranslate;
+
+    console.log(`${this.r1}  ${this.r2}`);
+  }
+
+  trueAnswer() {
+    if (this.r1 === this.r2) {
+      this.points += 10;
+      this.bonus += 1;
+    } else {
+      this.bonus = 0;
+    }
+
+    if (this.bonus >= 3) {
+      this.points += 20;
+    }
+    this.assignDataWords();
+  }
+
+  wrongAnswer() {
+    if (this.r1 !== this.r2) {
+      this.points += 10;
+      this.bonus += 1;
+    } else {
+      this.bonus = 0;
+    }
+
+    if (this.bonus >= 3) {
+      this.points += 20;
+    }
+    this.assignDataWords();
+  }
+
+  timeOfGame() {
+    const int = setInterval(() => {
+      this.timeOut -= 1;
+      if (this.timeOut === 0) {
+        clearInterval(int);
+        alert("geme over")
+      }
+    }, 1000);
+  }
 }
