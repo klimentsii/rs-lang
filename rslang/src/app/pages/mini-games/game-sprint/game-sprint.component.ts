bdfb31512e2a5251f01b5141888c8733af6b77b4
artvvr <artur.metelskiy@gmail.com>
2022-02-15 01:02:13 +0300

    feat: added combos, results and styling

--- rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
+++ rslang/src/app/pages/mini-games/game-sprint/game-sprint.component.ts
@@ -5,11 +5,25 @@ import { Location } from "@angular/common";
 import { dataBase } from "../../../interfaces/interfaces";
 import { URL, MAX_WORDS, MAX_DATA, LAST_PAGE, FIRST_PAGE, randN, elseRandN } from "../../../constants/constants";
 
+const TEN_POINTS = 10;
+
+const MAX_AMOUNT_FOR_BONUS = 3;
+
+const MIN_BONUS = 1;
+
+const ZEROING = 0;
+
+const ONE_SEC = 1;
+
+const MAX_SEC_OF_TIMER = 3;
+
+
 @Component({
   selector: "app-game-sprint",
   templateUrl: "./game-sprint.component.html",
   styleUrls: ["./game-sprint.component.css"]
 })
+
 export class GameSprintComponent implements OnInit {
 
   db: dataBase = [];
@@ -18,14 +32,31 @@ export class GameSprintComponent implements OnInit {
 
   wordTranslate!: string;
 
-  points: number = 0;
+  points = ZEROING;
 
-  bonus: number = 0;
+  bonus = ZEROING;
 
-  timeOut: number = 5;
+  timeOut = MAX_SEC_OF_TIMER;
+
+  combo = ZEROING;
 
   r1!: number;
+
   r2!: number;
+
+  saveCombo = MIN_BONUS;
+
+  showPopup?: boolean = false;
+
+  calcSumm!: number;
+
+  showSum?: boolean = false;
+
+  falseResultsWord: string[] = [];
+  falseResultsWordTranslate: string[] = [];
+
+  trueResultsWord: string[] = [];
+  trueResultsWordTranslate: string[] = [];
 
   constructor(protected _location: Location) { }
 
@@ -56,41 +87,102 @@ export class GameSprintComponent implements OnInit {
     console.log(`${this.r1}  ${this.r2}`);
   }
 
-  trueAnswer() {
-    if (this.r1 === this.r2) {
-      this.points += 10;
-      this.bonus += 1;
-    } else {
-      this.bonus = 0;
+  trueAnswer(): void {
+    const circle = document.querySelectorAll(".circle");
+    this.showSum = true;
+
+    if (this.combo === MAX_AMOUNT_FOR_BONUS) {
+      this.combo = ZEROING;
+      circle.forEach((v => v.classList.remove("green-coloring")));
     }
 
-    if (this.bonus >= 3) {
-      this.points += 20;
+
+    if (this.r1 === this.r2) {
+      this.calcSumm = TEN_POINTS;
+      circle[this.combo].classList.add("green-coloring");
+
+      this.calcSumm = TEN_POINTS * this.saveCombo;
+      if (circle[2].classList.contains("green-coloring")) {
+        this.saveCombo += MIN_BONUS;
+      }
+      this.points += this.calcSumm;
+      this.bonus += MIN_BONUS;
+      this.combo += MIN_BONUS;
+
+      this.trueResultsWord.push(this.word);
+      this.trueResultsWordTranslate.push(this.db[this.r1].wordTranslate);
+    } else {
+      this.falseResultsWord.push(this.word);
+      this.falseResultsWordTranslate.push(this.db[this.r1].wordTranslate);
+      this.bonus = ZEROING;
+      this.combo -= MIN_BONUS;
+      if (this.combo < ZEROING) this.combo = ZEROING;
+      circle[this.combo].classList.remove("green-coloring");
+      if (this.combo === ZEROING) {
+        this.saveCombo -= MIN_BONUS;
+        this.calcSumm -= TEN_POINTS;
+      }
+      if (this.saveCombo === ZEROING) {
+        this.saveCombo = MIN_BONUS;
+        this.calcSumm = ZEROING;
+      }
+      this.calcSumm = ZEROING;
+
     }
+
     this.assignDataWords();
   }
 
-  wrongAnswer() {
-    if (this.r1 !== this.r2) {
-      this.points += 10;
-      this.bonus += 1;
-    } else {
-      this.bonus = 0;
+  falseAnswer(): void {
+    const circle = document.querySelectorAll(".circle");
+    this.showSum = true;
+
+    if (this.combo === MAX_AMOUNT_FOR_BONUS) {
+      this.combo = ZEROING;
+      circle.forEach((v => v.classList.remove("green-coloring")));
     }
 
-    if (this.bonus >= 3) {
-      this.points += 20;
+    if (this.r1 !== this.r2) {
+      circle[this.combo].classList.add("green-coloring");
+
+
+      this.calcSumm = TEN_POINTS * this.saveCombo;
+      if (circle[2].classList.contains("green-coloring")) {
+        this.saveCombo += MIN_BONUS;
+      }
+      this.points += this.calcSumm;
+
+      this.bonus += MIN_BONUS;
+      this.combo += MIN_BONUS;
+      this.trueResultsWord.push(this.word);
+      this.trueResultsWordTranslate.push(this.db[this.r1].wordTranslate);
+    } else {
+      this.falseResultsWord.push(this.word);
+      this.falseResultsWordTranslate.push(this.db[this.r1].wordTranslate);
+      this.bonus = ZEROING;
+      this.combo -= MIN_BONUS;
+      if (this.combo < ZEROING) this.combo = ZEROING;
+      circle[this.combo].classList.remove("green-coloring");
+      if (this.combo === ZEROING) {
+        this.saveCombo -= MIN_BONUS;
+        this.calcSumm -= TEN_POINTS;
+      }
+      if (this.saveCombo === ZEROING) {
+        this.saveCombo = MIN_BONUS;
+      }
+      this.calcSumm = ZEROING;
+
     }
     this.assignDataWords();
   }
 
   timeOfGame() {
     const int = setInterval(() => {
-      this.timeOut -= 1;
-      if (this.timeOut === 0) {
+      this.timeOut -= ONE_SEC;
+      if (this.timeOut === ZEROING) {
         clearInterval(int);
-        alert("geme over")
+        this.showPopup = true;
       }
-    }, 1000);
+    }, ONE_SEC * 1000);
   }
 }
