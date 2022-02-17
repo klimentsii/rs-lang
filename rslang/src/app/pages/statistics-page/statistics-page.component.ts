e967654bcab8410d0fde2954e3fa3efc3bd5ecd3
artvvr <artur.metelskiy@gmail.com>
2022-02-17 16:40:20 +0300

    feat: added styles

--- rslang/src/app/pages/statistics-page/statistics-page.component.ts
+++ rslang/src/app/pages/statistics-page/statistics-page.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./statistics-page.component.css"]
 })
 export class StatisticsPageComponent implements OnInit {
-  titleGames: string[] = ["Sprint", "Audio-call"];
-  resInfo: string[] = ["Слов изучено", "Правильных ответов"];
-  constructor() { }
+  titleGames: string[] = ["sprint", "audio-call"];
+  resInfo: string[] = ["слов изучено", "правильных ответов"];
 
   ngOnInit(): void {
+    //
   }
 
 }
