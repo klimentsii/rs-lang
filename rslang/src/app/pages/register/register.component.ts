23cbd016cd59b1ea1dc9e1d7434b0d7e0367dc9c
klimentsii <klimparh@gmail.com>
2022-02-21 21:21:28 +0300

    fix: add added a limit on the number of errors on the registration and authorization pages

--- rslang/src/app/pages/register/register.component.ts
+++ rslang/src/app/pages/register/register.component.ts
@@ -68,6 +68,7 @@ export class RegisterComponent implements OnInit {
       border: 1px #F06171 solid; background-color: #4d010152; color: #F06171; padding: 10px 20px;
       margin: 10px; left: 0; border-radius: 10px; font-size: 24px; transition: all 0.5s
     `);
+    this.more3();
     return el;
   }
 
@@ -79,6 +80,13 @@ export class RegisterComponent implements OnInit {
       border: 1px #4FEE97 solid; background-color: #4d010152; color: #4FEE97; padding: 10px 20px;
       margin: 10px; left: 0; border-radius: 10px; font-size: 24px; transition: all 0.5s
     `);
+    this.more3();
     return el;
+  }
+
+  more3() {
+    if (Number(document.querySelector(".auth")?.childElementCount) > 5) {
+      document.querySelector(".auth")?.children[1].remove();
+    }
   }
 }
